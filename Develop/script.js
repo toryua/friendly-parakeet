3bff1b062126d52f45a43279c635da88ed264f4c
toryua <87789457+toryua@users.noreply.github.com>
2021-08-22 13:45:37 -0700

    added language for passwordLength; unsure where to put it

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,52 @@
 // Assignment code here
-
+// Password Prompt Functions
+var passwordLength = window.prompt("Input a length for your password that is between 8 and 128", "10") {
+  if (passwordLength < 8) or if (passwordLength > 128) {
+    window.alert("Invalid input: Password must be between 8 and 128 characters");
+    else console.log passwordLength;
+    
+  }
+}
+// verify that the user's value is between 8 and 128
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // present user with series of prompts for password criteria
+
+  // prompt #1: password length
+  var passwordLength = window.prompt("Input a length for your password that is between 8 and 128", "10") {
+    if (passwordLength < 8) or if (passwordLength > 128) {
+      window.alert("Invalid input: Password must be between 8 and 128 characters");
+      else console.log passwordLength;
+      
+    }
+  }
+  
   passwordText.value = password;
   
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button; runs writePassword function 
+generateBtn.addEventListener("click", writePassword) {
+  var passwordLength = window.prompt("Input a length for your password that is between 8 and 128", "10") {
+    if (passwordLength < 8) or if (passwordLength > 128) {
+      window.alert("Invalid input: Password must be between 8 and 128 characters");
+      else console.log passwordLength;
+      
+    }
+  }
+}
+
+
+
+
+
+
