ef6fbdb09ccff54a7d5c81bdec7d7863d7a9e70c
toryua <87789457+toryua@users.noreply.github.com>
2021-08-22 18:26:03 -0700

    prompt #1 start functions

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,6 @@
 // Password Prompt Functions
 
 
-// verify that the user's value is between 8 and 128
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -11,34 +9,52 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  // present user with series of prompts for password criteria
+  // generatePassword function will present user with series of prompts 
+  // for password criteria and is located within the scope of writePassword. 
+  // after the prompts are satisfied, the function will then 
+  // generate the password to be called by the parent writePassword function. 
 
-  // prompt #1: password length
-  var passwordLength = window.prompt("Input a length for your password that is between 8 and 128", "10") {
-    if (passwordLength < 8) or if (passwordLength > 128) {
-      window.alert("Invalid input: Password must be between 8 and 128 characters");
-      else console.log passwordLength;
-      
-    }
-  }
-  
-  passwordText.value = password;
-  
-}
 
-// Add event listener to generate button; runs writePassword function 
-generateBtn.addEventListener("click", writePassword) {
-  var passwordLength = window.prompt("Input a length for your password that is between 8 and 128", "10") {
-    if (passwordLength < 8) or if (passwordLength > 128) {
-      window.alert("Invalid input: Password must be between 8 and 128 characters");
-      else console.log passwordLength;
-      
-    }
-  }
-}
+  var password = generatePassword(
+
+
+    // prompt #1: password length 
+    function passwordLength() {
+      var passwordLength = Number(window.prompt("Input your desired password length (equal to or between 8 and 128)", "10"));
+      if (passwordLength >= 8 && passwordLength <= 128) {
+        var lowercase = window.prompt("Do you want lowercase letters?");
+        if (lowercase) {
+          window.alert("Lowercase letters will be added to your password.")
+        };
+        // else {
+        window.alert("Lowercase letters will not be added to your password.")
+      }
+      else {
+        window.alert("Invalid input: Password must be between 8 and 128 characters");
+        passwordLength();
+      }
+     }
+
+
+
+      ;
+      var passwordText = document.querySelector("#password");
+
+
+
+
+      passwordText.value = password;
+
+
+
+
+      // Add event listener to generate button; runs writePassword function 
+      generate.addEventListener(click, writePassword());
+
+
+
+
 
 
 
